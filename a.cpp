f594d8d0da1ccf19337e8ec949e25d5c4ca1ea76
mabuchi <mabuchi.ei@gmail.com>
2025-06-19 16:45:33 +0900

    Auto Update 2025-06-19 16:45:33

--- a.cpp
+++ a.cpp
@@ -4,22 +4,6 @@
 using namespace std;
 
 int main(){
-    int M,N,ans;
-    cin >> M >> N;
-    vector <char> C(N);
-    for(int i=0;i<N;i++) cin >> C[i];
-    vector <vector <int> > Data(M,vector <int> (N));
-    for(int i=0;i<M;i++){
-        for(int j=0;j<N;j++){
-            cin >> Data[i][j];
-        }
-    }
-    for(int i=0;i<N;i++){
-        ans=0;
-        for(int j=0;j<M;j++){
-            ans+=Data[j][i];
-        }
-        cout << C[i] << " " << ans/M << endl;
-    }
+    
     return 0;
 }
