6fd34cf08dce3017c8568e14f2e24459e407297e
ei2537 <mabuchi.ei@gmail.com>
2025-06-20 10:58:49 +0900

    Auto Update 2025-06-20 10:58:49

--- git-pull-auto.sh
+++ git-pull-auto.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-cd ~/VSCode || exit 1
+cd ~/VSCode || exit 1  
 
-# === プロキシ自動設定（学校だけ） ===
-if grep -q "hamako-ths" /etc/resolv.conf; then
-    export http_proxy="http://proxy.hamako-ths.ed.jp:8080"
-    export https_proxy="http://proxy.hamako-ths.ed.jp:8080"
-    echo "[Proxy] School proxy settings are being applied..."
-else
-    unset http_proxy
-    unset https_proxy
-    echo "[Proxy] Home."
-fi
-
-# === Git 自動pull ===
 echo "[Git] Auto pull..."
+
 git pull --rebase
+
 echo "[Git] Done."
