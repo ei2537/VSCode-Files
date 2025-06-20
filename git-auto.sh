2f97af4cddb03fda3118607178f47a81d96158f5
ei2537 <mabuchi.ei@gmail.com>
2025-06-20 20:37:54 +0900

    Auto Update 2025-06-20 20:37:54

--- git-auto.sh
+++ git-auto.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd ~/VSCode || exit 1
+DIR=$(pwd)
+
+echo "[Git] Working in $DIR..."
+cd "$DIR" || exit 1
 
 echo "[Git] Checking for changes..."
 git fetch
@@ -8,26 +11,20 @@ if git diff --quiet && git diff --cached --quiet; then
     echo "[Git] No changes. Getting latest information from remote..."
     git pull --rebase
 else
-    echo "[Git] Detecting changes in the working tree. Skip pull."
+    echo "[Git] Detected changes in working tree. Skipping pull."
 fi
 
 echo "[Git] Staging changes..."
 git add .
 
 if git diff --cached --quiet; then
-    echo "[Git] No changes. Skip commit and push."
+    echo "[Git] No changes to commit."
 else
     read -p "Commit Message: " msg
-    if [[ -z "$msg" ]]; then
-        msg="Auto Update $(date '+%Y-%m-%d %H:%M:%S')"
-    fi
+    [[ -z "$msg" ]] && msg="Auto Update $(date '+%Y-%m-%d %H:%M:%S')"
     git commit -m "$msg"
-
-    echo "[Git] Pushing to GitHub..."
-    git push --set-upstream origin main
-
-    echo "[Git] Also pushing to Organization (ei2537-org)..."
-    git push org main
+    echo "[Git] Pushing to origin..."
+    git push
 fi
 
 echo "[Git] Done."
