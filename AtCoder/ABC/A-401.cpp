49682728aeed960a4082e8bbd8d663957f5ba14c
mabuchi <mabuchi.ei@gmail.com>
2025-07-03 11:41:04 +0900

    Auto Update 2025-07-03 11:41

---
+++ AtCoder/ABC/A-401.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#define endl "\n"
+using namespace std;
+
+int main(){
+    int S;
+    cin >> S;
+    if(200<=S&&S<=299) cout << "Success" << endl;
+    else cout << "Failure" << endl;
+    return 0;
+}
